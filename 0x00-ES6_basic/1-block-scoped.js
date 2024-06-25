1891320569f8f5a1b8965ee0b8d5e45eaed041ea
Yanga Rubushe <zilithemba10@gmail.com>
2024-06-25 07:49:58 +0000

    🔧

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,4 +1,3 @@
-/* eslint-disable */
 export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
@@ -6,6 +5,9 @@ export default function taskBlock(trueOrFalse) {
   if (trueOrFalse) {
     const task = true;
     const task2 = false;
+    if (task && task2) {
+      return [task, task2];
+    }
   }
 
   return [task, task2];
